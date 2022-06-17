2a5a2f0ac869c1642e49ccad4170fcfa42fbe3da
auliiaasr <89631647+auliiaasr@users.noreply.github.com>
2022-06-17 19:53:47 +0700

    progress 12

--- connect.php
+++ connect.php
@@ -8,6 +8,7 @@ if ($server) {
     define('pass', 'secret');
     define('db', 'uas202410101122');
     $baseUrl = 'http://pbw.ilkom.unej.ac.id/sia/sia202410101122/UAS/';
+    $baseDir = '/home/sia202410101122';
 } else {
     // LOCAL //
     define('host', 'localhost');
@@ -15,6 +16,7 @@ if ($server) {
     define('pass', '');
     define('db', 'project');
     $baseUrl = '';
+    $baseDir = '';
 }
 
 $conn = mysqli_connect(host, user, pass, db);
